c02633b67af821a94e614b1dff639443672ca478
Corbin Robb <corbinrobb@gmail.com>
2020-04-13 16:51:02 -0600

    Attempt more stretch goals

--- src/App.js
+++ src/App.js
@@ -9,14 +9,29 @@ function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
   const [ homeScore, setHomeScore ] = useState(0);
   const [ awayScore, setAwayScore ] = useState(0);
+  const [ quarter, setQuarter ] = useState(1);
+  const [ down, setDown ] = useState(1);
+  const [ toGo, setToGo ] = useState(0);
+  const [ ballOn, setBallOn ] = useState(0);
 
   return (
     <div className="container">
       <section className="scoreboard">
         <TopRow homeScore={homeScore} awayScore={awayScore} />
-        <BottomRow />
+        <BottomRow quarter={quarter} down={down} toGo={toGo} ballOn={ballOn}/>
       </section>
-        <Buttons homeScore={homeScore} awayScore={awayScore} setHomeScore={setHomeScore} setAwayScore={setAwayScore} />
+        <Buttons 
+          homeScore={homeScore}
+          awayScore={awayScore} 
+          setHomeScore={setHomeScore} 
+          setAwayScore={setAwayScore}
+          quarter={quarter}
+          setQuarter={setQuarter}
+          down={down}
+          setDown={setDown}
+          setToGo={setToGo}
+          setBallOn={setBallOn}
+        />
     </div>
   );
 }
